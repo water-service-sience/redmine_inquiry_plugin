0c3623824ebfc7c4e472416bc2cdc96ec99f8093
takeshita <takeshita@geishatokyo.com>
2012-09-17 17:54:36 +0900

    i18n

--- init.rb
+++ init.rb
@@ -9,8 +9,8 @@ Redmine::Plugin.register :inquiry do
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
   
-  menu :project_menu, :geo_info, { :controller => "geo_info", :action => "index"},:caption => "GeoInfo", :param => :project_id
-  menu :project_menu, :customer, { :controller => "customer", :action => "index"},:caption => "Customer", :param => :project_id
+  menu :project_menu, :geo_info, { :controller => "geo_info", :action => "index"},:caption => "geo_info", :param => :project_id
+  menu :project_menu, :customer, { :controller => "customer", :action => "index"},:caption => "customer", :param => :project_id
   project_module :inquiry do
     permission :geo_viewer, :geo_info => [:index]
     permission :customer_viewer, :customer => [:index]
